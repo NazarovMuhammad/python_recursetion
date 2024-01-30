24b044a3534bf31e69c6676b0e6a1512195f953f
NazarovMuhammad <nazarovm4464@gmail.com>
2024-01-30 18:14:38 +0500

    first commit

---
+++ tasks.py
@@ -0,0 +1,26 @@
+# s = input()  
+# max_namber = max(s)  
+# print(max_namber)  
+
+# s = input()
+# cnt = 0
+# for i in s:
+#     if i.isdigit():
+#         cnt += 1
+# print(cnt)
+
+
+# a = int(input())
+# b = int(input())
+# javob_a = 0
+# javob_b = 0
+# for i in range(1,a+1):
+#     if a%i==0:
+#       javob_a = i
+# for j in range(1,b+1):
+#     if b%j==0:
+#         javob_b = j
+# for k in javob_a:
+#     if javob_b == k:
+#         print(k)
+
